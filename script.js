5ac1cb32153a4a3345b749d370150a9fe9691fd9
Huong <huongnguyen.nth1709@gmail.com>
2024-03-21 16:54:49 +0200

    add function to generate random hexa code and set gradient background base on these color codes

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ var h3 = document.querySelector('h3');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 const body = document.getElementById('gradient');
+const generateBtn = document.querySelector('.button-40');
 
-setGradient();
+randomGradientBg();
+
+function randomHexColorCode() {
+  let n = (Math.random() * 0xfffff * 1000000).toString(16);
+  return '#' + n.slice(0, 6);
+}
+
+// setGradient();
 
 function setGradient() {
   body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
   h3.textContent = body.style.background + ';';
 }
 
+function randomGradientBg() {
+  color1.value = randomHexColorCode();
+  color2.value = randomHexColorCode();
+  setGradient();
+}
+
 color1.addEventListener('input', setGradient);
 
 color2.addEventListener('input', setGradient);
+
+generateBtn.addEventListener('click', randomGradientBg);
